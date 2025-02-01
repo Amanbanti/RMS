3d206e6bf601d904658d193a37e65cb9f8e57d20
Amanbanti <amanbanti2011@gmail.com>
2025-02-01 22:44:33 +0300

    user routing commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+    authUser,
+    registerUser,
+    logoutUser ,
+    getUserById,
+    getUsers,
+    deleteUser,
+    updateUser 
+    
+    } from '../controllers/userController.js'
+
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+
+router.get('/',protect,admin,getUsers);
+router.get('/:id',protect,admin, getUserById);
+router.post('/',registerUser);
+router.post('/logout',logoutUser);
+router.post('/login', authUser);
+router.delete('/:id',protect,admin,  deleteUser);
+router.put('/:id',protect,admin,  updateUser);
+
+
+export default router;
