5d9df7d92c99c11959762913ed8e819b7d73321d
Amanbanti <amanbanti2011@gmail.com>
2025-02-05 16:58:53 +0300

    updateing the slidebars

--- frontend/src/components/UserRoutes.jsx
+++ frontend/src/components/UserRoutes.jsx
@@ -3,9 +3,10 @@ import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoute = () => {
   const user = JSON.parse(localStorage.getItem("userInfo")); // Get user from local storage
-  const hasToken = document.cookie.includes("jwt"); // Check if JWT cookie exists
 
-  return user && hasToken ? <Outlet /> : <Navigate to="/login" replace />;
+  console.log(user)
+
+  return user ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
