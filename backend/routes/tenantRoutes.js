3554e430cd70ee5912131661abe76464f2348e90
Amanbanti <amanbanti2011@gmail.com>
2025-02-11 19:04:19 +0300

    tenantRoutes commit

--- backend/routes/tenantRoutes.js
+++ backend/routes/tenantRoutes.js
@@ -1,15 +1,28 @@
-import express from "express";
-import {
- registerTenant
 
-    } from '../controllers/tenantControllers.js'
+import express from "express";
+import { registerTenant } from "../controllers/tenantControllers.js";
+import multer from "multer";
+import fs from "fs";
 
 
 const router = express.Router();
 
-router.post('/register', registerTenant);
+// Ensure "uploads" folder exists
+const uploadDir = "uploads/tenants/";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
+// Multer setup for storing files
+const storage = multer.diskStorage({
+  destination: uploadDir,
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
+const upload = multer({ storage });
 
+router.post("/register", upload.single("identificationDocument"), registerTenant);
 
 export default router;
